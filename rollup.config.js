9b58ef0e10c600fb5db4edd3cc59876de49f5947
Maxim Salnikov <salnikov@gmail.com>
2024-11-08 13:45:38 +0100

    rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,19 @@
-import resolve from '@rollup/plugin-node-resolve'
-import replace from '@rollup/plugin-replace'
-import { terser } from '@rollup/plugin-terser'
+import resolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import terser from "@rollup/plugin-terser";
 
 export default {
-  input: 'dist/ng-ai/sw.js',
+  input: "dist/ng-ai/sw.js",
   output: {
-    file: 'dist/ng-ai/sw.js',
-    format: 'iife'
+    file: "dist/ng-ai/sw.js",
+    format: "cjs",
   },
   plugins: [
     resolve(),
     replace({
-      'process.env.NODE_ENV': JSON.stringify('development')
+      "process.env.NODE_ENV": JSON.stringify("development"),
+      preventAssignment: true,
     }),
-    terser()
-  ]
-}
+    terser(),
+  ],
+};
